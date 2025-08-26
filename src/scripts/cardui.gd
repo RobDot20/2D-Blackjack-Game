0865d84e67b7622676cf9255fe2863a35a83834a
RobDot20 <peperoby10@gmail.com>
2025-08-27 00:41:40 +0300

    start card ui
    nu am apucat sa fac nimic dar am scris ideile intr-un comentariu in scriptul de la card ui

---
+++ src/scripts/cardui.gd
@@ -0,0 +1,10 @@
+extends NinePatchRect
+
+@export var card_data : PlayingCard
+
+#vreau sa iau fiecare cifra si litera si sa le dau preload aici in scriptul asta ca sa schimb in functie de ce
+#carte primeste in export
+#la fel si la fiecare imagine ca mi se pare mai usor decat sa stau sa misc regiunea de la nod ul asta pt fiecare
+#carte in parte, dar se poate scripta si asta daca nu reusim ig
+#ar fi o idee sa transformam toate zonele albe dintre carti in spatiu gol sa ramanem doar cu desenele si dupa le
+#dam load
