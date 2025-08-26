b6fe84ce03960144aef7277cdebf32f892e421a7
paleski <paleski420@gmail.com>
2025-08-26 22:50:13 +0300

    Adaugare goals ce urmeaza la sfarsitul deck.gd

--- src/scripts/deck.gd
+++ src/scripts/deck.gd
@@ -70,3 +70,10 @@ func _init(add_joker : bool) :
 			new_card.ownership = 0
 			deck.append(new_card)
 			print("joker function called")
+			
+# DE URMAT: FUNCTIE CARE IA 2 POZITII DE CARTI RANDOM DIN ARRAY SI LE DA OWNERSHIP = 1 SI DUPA LA FEL NUMAI CA OWNERSHIP = 3
+# FUNCTIE CARE SA PARCURGA ARRAYUL SI SA DEA PRINT LA CARTILE CU OWNERSHIP = 1, OWNERSHIP = 2 (daca nu este null), OWNERSHIP = 3
+# FUNCTIE CARE SA PARCURGA ARRAYUL, DACA DOUA CARTI CU OWNERSHIP = 1 AU ACCEASI VALUE, DA PROMPT LA OPTIUNEA SPLIT
+# FUNCTIE CARE MODIFICA VALOAREA DE OWNERSHIP A CELEI DA DOUA CARTI A PLAYERULUI LA VALOAREA OWNERSHIP = 2
+# ----
+# dupa ce toate cerintele de mai sus au fost indeplinite, trecem la sistemul de hit (unde va trebui sa includem si split daca e cazul)
